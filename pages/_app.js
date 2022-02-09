6b7c3820309593d114012292bec8bc8847cddb69
abhik-b <abhik0301@gmail.com>
2022-02-09 07:54:13 +0530

    added authentication & chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,52 @@
-import '../styles/globals.css'
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+
+import { AuthProvider } from '@/lib/auth';
+import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+    fonts: {
+        body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`
+    },
+    fontWeights: {
+        normal: 400,
+        medium: 600,
+        bold: 700
+    },
+    colors: {
+        brand: {
+            900: '#1a365d',
+            800: '#153e75',
+            700: '#2a69ac'
+        }
+    },
+    styles: {
+        global: {
+            'html, body': {
+                'min-width': '360px',
+                'scroll-behavior': 'smooth'
+            },
+            '#__next': {
+                display: 'flex',
+                'flex-direction': 'column',
+                'min-height': '100vh'
+            }
+        }
+    }
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ChakraProvider theme={theme}>
+            <AuthProvider>
+                {' '}
+                <Component {...pageProps} />
+            </AuthProvider>
+        </ChakraProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
